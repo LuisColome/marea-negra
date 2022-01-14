7827208722b3bb8e44532f486bb64da7fd8d9df1
Luis Colomé <hello@luiscolome.com>
2022-01-14 11:26:11 +0100

    Add the site logo + Fix Google fonts

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ add_action( 'enqueue_block_editor_assets', 'ea_gutenberg_scripts' );
 *
 */
 function lcm_theme_fonts_url() {
-	return 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&family=Kumbh+Sans:wght@300;400;600&family=Lexend:wght@600&display=swap';
+	return 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap';
 }
 
 function ea_child_theme_setup() {
@@ -70,7 +70,7 @@ function ea_child_theme_setup() {
 	include_once( get_stylesheet_directory() . '/inc/genesis-changes.php' );
 
 	// Theme
-	//include_once( get_stylesheet_directory() . '/inc/site-logo.php' );
+	include_once( get_stylesheet_directory() . '/inc/site-logo.php' );
 	include_once( get_stylesheet_directory() . '/inc/site-footer.php' );
 	include_once( get_stylesheet_directory() . '/inc/login-logo.php' );
 	include_once( get_stylesheet_directory() . '/inc/layouts.php' );
