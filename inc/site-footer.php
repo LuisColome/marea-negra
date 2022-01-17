18d54d4cbd079b99a82c1388f3cc24ea4116ad02
Luis Colomé <hello@luiscolome.com>
2022-01-17 16:13:27 +0100

    Update files

--- inc/site-footer.php
+++ inc/site-footer.php
@@ -8,13 +8,15 @@
  * @license      GPL-2.0+
 **/
 
+//* Enable the block-based widget editor
+add_filter( 'use_widgets_block_editor', '__return_true' );
+
 /**
  * Site Footer
  */
 function lcm_site_footer() {
 	echo '<div class="footer-left">';
 		echo '<p class="copyright">Copyright &copy; ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) . ' ®. All Rights Reserved.</p>';
-		echo '<p class="footer-links"><a href="' . home_url( 'privacy-policy' ) . '">Privacy Policy</a> <a href="' . home_url( 'terms' ) . '">Terms</a></p>';
 	echo '</div>';
 	// echo '<a class="backtotop" href="#">Back to top' . ea_icon( array( 'icon' => 'arrow-up' ) ) . '</a>';
 }
